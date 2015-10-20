43671b2db6ac82e6740529a0735c6f4caed7d12f
Paco Martin Fernandez <erespia2@gmail.com>
2015-10-20 12:48:48 +0300

    Add Google Analytics

--- services/configuration.js
+++ services/configuration.js
@@ -12,7 +12,7 @@ var config  = require('../configuration.js');
 
 exports.init = function(app){
 
-	initLeagueBBVA();
+	//initLeagueBBVA();
 
 	// TODO: Para lanzar el <node jobs> hace falta esperar que en el <node app> aparezcan los siguientes mensajes
 	//       Pueden aparecer en distinto orden, ya que son asincronos (solo algunos, a partir de Players Added)
@@ -58,7 +58,7 @@ function initLeagueBBVA(){
 
 	console.log("Importing Liga BBVA Data...");
 
-	importData.addLeague(nameLeague, countryLeague, divisionLeague, aTeams, './services/assets/playersBBVA.json', year, web);
+	importData.addLeague(nameLeague, countryLeague, divisionLeague, aTeams, './services/assets/playersBBVALite.json', year, web);
 }
 
 // MOCK DATA, DELETE IN RELEASE VERSION
